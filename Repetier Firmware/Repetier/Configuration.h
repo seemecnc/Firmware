c5589e66b90fba09e54fc79e92c85d28254984c8
John Oly <johnoly99@gmail.com>
2016-06-14 07:42:38 -0400

    added droplit config defaults for firmware to compile
    added probe related configs for droplit with false/null values to get
    firmware to compile

--- Repetier Firmware/Repetier/Configuration.h
+++ Repetier Firmware/Repetier/Configuration.h
@@ -533,6 +533,26 @@ WARNING: Servos can draw a considerable amount of current. Make sure your system
 #define Z_PROBE_FINISHED_SCRIPT ""
 #define FEATURE_AUTOLEVEL 1
 
+#elif PRINTER == 4  // DropLit doesn't have a probe yet ;)
+#define FEATURE_Z_PROBE 0
+#define Z_PROBE_SENSITIVITY  25 // 0-126 7 bit value  
+#define Z_PROBE_BED_DISTANCE 20
+#define Z_PROBE_PIN 16 // mini-rambo ext pins on P3 ext next to LCD header
+#define Z_PROBE_PULLUP 1 //0
+#define Z_PROBE_ON_HIGH 0 //1
+#define Z_PROBE_X_OFFSET 0
+#define Z_PROBE_Y_OFFSET 0
+#define Z_PROBE_WAIT_BEFORE_TEST 0
+#define Z_PROBE_SPEED 90
+#define Z_PROBE_XY_SPEED 50
+#define Z_PROBE_SWITCHING_DISTANCE 10
+#define Z_PROBE_REPETITIONS 1
+#define Z_PROBE_HEIGHT -.2
+#define Z_PROBE_START_SCRIPT "G28/nG1Z25/n"
+//#define Z_PROBE_START_SCRIPT "M117 Probe Started/n"
+#define Z_PROBE_FINISHED_SCRIPT ""
+#define FEATURE_AUTOLEVEL 1
+
 #endif
 
 // ##############  Z Probe X/Y coordinates for endstop offsets and radius comp uses the Z_PROBE_Y3 coordinate ############
@@ -560,6 +580,14 @@ WARNING: Servos can draw a considerable amount of current. Make sure your system
 #define Z_PROBE_Y2 -31 
 #define Z_PROBE_X3   0
 #define Z_PROBE_Y3  65 
+
+#elif PRINTER == 4  //  DROPLIT false values for firmware to compile
+#define Z_PROBE_X1 0 
+#define Z_PROBE_Y1 0 
+#define Z_PROBE_X2 0
+#define Z_PROBE_Y2 0 
+#define Z_PROBE_X3 0
+#define Z_PROBE_Y3 0 
 
 #endif
 
